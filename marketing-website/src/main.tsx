b12b5f0240276ba8c87866d8933cf6e7d9042858
Matt Kiselstein <mkiselstein@gmail.com>
2025-09-07 13:18:31 -0700

    Fix import path in marketing-website

--- marketing-website/src/main.tsx
+++ marketing-website/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import App from './app';
+import App from './app.js';
 import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
