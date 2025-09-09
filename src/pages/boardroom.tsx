33759468d740da7aed79c530c0a7a521c5e3775a
Matt Kiselstein <mkiselstein@gmail.com>
2025-09-09 15:07:49 -0700

    Change header from 'The Collective' to 'Boardroom'

--- src/pages/boardroom.tsx
+++ src/pages/boardroom.tsx
@@ -438,7 +438,7 @@ const Boardroom = () => {
       <div className="max-w-[1500px] mx-auto w-full px-4 md:px-6 lg:px-8">
         <div className="pt-8">
           <PageHeader 
-            title="The Collective" 
+            title="Boardroom" 
             subtitle="12 expert personas. One decisive answer."
           />
           
